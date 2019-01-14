75b2386af1aeda09da4290f56d82981f4aa00779
tianyu <tianfwork@163.com>
2019-01-14 21:33:25 +0800

    merge

---
+++ src/main/java/com/imooc/day03_LinkedList/Main.java
@@ -0,0 +1,11 @@
+package com.imooc.day03_LinkedList;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> integers = new LinkedList<Integer>();
+        boolean add = integers.add(1);
+        System.out.println(add);
+    }
+}
