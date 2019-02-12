5ec4063f4d29e84d05b196cab01ec2a254669b58
tianyu <tianyu@hualala.com>
2019-02-12 14:40:22 +0800

    二分搜索树完结

--- src/main/java/com/imooc/day04_BST/Main.java
+++ src/main/java/com/imooc/day04_BST/Main.java
@@ -4,17 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
         BST<Integer> bst = new BST<Integer>();
-        int[] array = {28, 23, 45, 67, 13, 2, 89};
+        /*int[] array = {28, 23, 45, 67, 13, 2, 89};
 
         for (int i = 0; i < array.length; i++) {
             bst.add(array[i]);
-        }
+        }*/
 
 //        bst.preOrder();
 //        System.out.println(bst);
 //        bst.inOrder();
 //        bst.postOrder();
 //        bst.preOrderNR();
-        bst.levelOrder();
+//        bst.levelOrder();
+        bst.add(28);
+        bst.add(33);
+        bst.add(29);
+        bst.add(26);
+        bst.add(20);
+        bst.add(30);
+        bst.add(25);
+//        bst.preOrder();
+        bst.inOrder();
     }
 }
